e79ea9a53c96f676778612d23ef5a1a1af0e1a92
Huy Nguyen <huy.nguyen@pfizer.com>
2024-10-31 08:52:11 +0700

    feat: add header component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
+import { Header } from "@/components/Header/Header";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import Container from "@mui/material/Container";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <AppRouterCacheProvider>
-          <main>{children}</main>
+          <Container maxWidth="lg">
+            <Header />
+          </Container>
+          <main>
+            <Container maxWidth="lg">{children}</Container>
+          </main>
         </AppRouterCacheProvider>
       </body>
     </html>
